aef717ba655a6ecac9e11eaba723de6eb8e3fb36
M. Hammad Shahid <hammadshahid863@yahoo.com>
2025-08-14 06:06:08 -0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = "Generative AI project",
+    version = "0.0.0",
+    author = "M.Hammad Shahid",
+    author_email = "hammadshahid863@yahoo.com",
+    packages = find_packages(),
+    install_requires = []
+)
---
+++ template.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message):')
+
+list_of_files = [
+    "src/_init_.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "app.py",
+    "research/trails.ipynb",
+    "test.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating doirectory; {filedir} for the file : {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0 ):
+        with open(filepath, "w") as f:
+            logging.info(f"Creating enpty file: {filepath}")
+            
+    else:
+        logging.info(f"{filename} already exists")
