5ff903b72576ca90b877468b7e8cc46a3c0e2017
M. Hammad Shahid <hammadshahid863@yahoo.com>
2025-10-04 01:11:31 -0700

    Fix: Flask working locally with port 8000

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def chat():
 # Run app
 # ------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # use Render's PORT if available
+    port = int(os.environ.get("PORT", 8000))  # use Render's PORT if available
     app.run(host="0.0.0.0", port=port)
